c8596d3ae6eab8f840457c8f68dfc10b591b6033
Marek Chałabis <chalabismarek@gmail.com>
2020-05-14 15:50:40 +0200

    Added REST API v1.1

---
+++ api/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, include
+from .views import PatientViewSet, DonationViewSet, UserViewSet, PublicInfoViewSet
+from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views
+
+router = DefaultRouter()
+router.register('patients', PatientViewSet, basename='patients')
+router.register('donations', DonationViewSet, basename='donations')
+router.register('users', UserViewSet, basename='users')
+router.register('public', PublicInfoViewSet, basename='public')
+
+
+urlpatterns = [
+    # view set robi aoutoatycznie url
+    path('v1/', include(router.urls)),
+    path('get-token', views.obtain_auth_token)
+]
+
